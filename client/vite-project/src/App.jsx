e95b03c9ba86dd6cf4dd3696e2256e676bdc54c1
patilarya76 <patilaryaanil07@gmail.com>
2026-09-09 21:54:03 +0530

    Fix case-sensitive import of reels.jsx for Linux deployment

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -7,7 +7,7 @@ import {
 
 import Home from "./pages/homepage.jsx";
 import Login from "./pages/loginpage.jsx";
-import Reels from "./pages/Reels.jsx";
+import Reels from "./pages/reels.jsx";
 import AllProducts from "./pages/AllProduct.jsx";
 import Navbar from "../src/components/navbar.jsx";
 import ProtectedRoute from "../src/pages/admin/Protectedroute.jsx"; 
